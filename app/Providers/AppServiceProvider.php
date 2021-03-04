b72da9604982bce38d1330b22678f7d44e327b46
salmalifias <47998386+salmalifias@users.noreply.github.com>
2021-03-04 12:41:14 +0700

    hbjh

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(config('app.debug')!=true) {
-            \URL::forceScheme('https');
-          }
+        
     }
 }
